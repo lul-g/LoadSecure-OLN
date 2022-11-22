8dcc0bdc865d08f4f99bccb0e718296f0a98ca40
Lulseged Admasu <lulsegedwork@gmail.comu>
2022-11-21 19:09:27 -0500

    fixed nav issues related to txt-clr

--- assets/js/active.js
+++ assets/js/active.js
@@ -86,6 +86,19 @@ video.onended = () => {
 //   }
 // }
 
+// *nav-bar txt colors on window resize
+let nav_links = document.querySelectorAll(".nav-txt a");
+window.addEventListener("resize", () => {
+  if (screen_size.width <= 750) {
+    nav_links.forEach((nav_link) => {
+      nav_link.style.color = "#dae4f1";
+    });
+  } else {
+    nav_links.forEach((nav_link) => {
+      nav_link.style.color = "#455c66";
+    });
+  }
+});
 //* to-top-btn
 let to_top_btn = document.querySelector(".to-top-btn");
 to_top_btn.addEventListener("click", () =>
@@ -94,7 +107,6 @@ to_top_btn.addEventListener("click", () =>
 
 // * scroll header color change  || to-top-btn in-out of view
 let section_header = document.querySelector(".section_header");
-let nav_links = document.querySelectorAll(".nav-txt a");
 let extra_nav = document.querySelector(".extra_nav");
 let extra_nav_child = document.querySelectorAll(".extra_nav_child");
 let glow_btn = document.querySelectorAll(".glow-btn");
